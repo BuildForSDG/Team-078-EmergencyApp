d489b6fdf4bcd41b2561e53ef94f1ad11a48f415
TonykayNoni <tonykay001@gmail.com>
2020-05-12 17:58:40 +0000

    Alert Response for Respondant Login

--- src/app/pages/admin-pages/admin-login/admin-login.page.ts
+++ src/app/pages/admin-pages/admin-login/admin-login.page.ts
@@ -65,24 +65,5 @@ export class AdminLoginPage implements OnInit {
       } );
     }
   }
-  /**
-  loginUser(){  
-    return this._auth.loginUser(this.loginCredentials.email,this.loginCredentials.password).then((result) => {
-      alert('You have been successfully logged in!');  
-      this.router.navigate(['/admin-dashboard']);  
-    }).catch(error => { 
-    console.log("Error",error); 
-    alert("Error: " + error.message );
-    throw new Error(error); 
-  }); 
-    // if(login){
-    //   alert('Login Successful');
-    // }else{
-    //   alert('Login Failed')
-    // }
-
-    // console.log(login);
  
- }
-  */
 }
