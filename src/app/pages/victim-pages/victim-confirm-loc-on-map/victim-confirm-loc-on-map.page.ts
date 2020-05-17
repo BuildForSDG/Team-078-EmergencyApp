203926d0b4481cb883a398f4f651d567ab05f20d
TonykayNoni <tonykay001@gmail.com>
2020-05-17 23:57:56 +0000

    fix: route issue

--- src/app/pages/victim-pages/victim-confirm-loc-on-map/victim-confirm-loc-on-map.page.ts
+++ src/app/pages/victim-pages/victim-confirm-loc-on-map/victim-confirm-loc-on-map.page.ts
@@ -3,13 +3,13 @@ import { AlertController, LoadingController } from "@ionic/angular";
 import { Plugins } from "@capacitor/core";
 import { Router, ActivatedRoute } from "@angular/router";
 import { GoogleMapComponent } from "../../../components/google-map/google-map.component";
-import { AuthService } from '../../../services/user/auth.service';
+import { AuthService } from "../../../services/user/auth.service";
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
 const { Geolocation, Storage } = Plugins;
-declare	var	google;
+var google;
 @Component({
   selector: "app-victim-confirm-loc-on-map",
   templateUrl: "./victim-confirm-loc-on-map.page.html",
@@ -24,22 +24,22 @@ export class VictimConfirmLocOnMapPage implements OnInit {
   public loading: HTMLIonLoadingElement;
   data: any;
   markerlatlong = {
-    lat : 0,
-    long : 0,
-  }
-  userInfo = {
-    emmergency: '',
-    address: '',
-    latLong: this.markerlatlong,
-    phone_number: ''
+    lat: 0,
+    long: 0
   };
-  
+  userInfo = {
+    emmergency: "",
+    address: "",
+    latLong: this.markerlatlong,
+    phone_number: ""
+  };
+
   constructor(
     private route: ActivatedRoute,
     private alertCtrl: AlertController,
     private loadingCtrl: LoadingController,
     private router: Router,
-    private _auth: AuthService,
+    private _auth: AuthService
   ) {
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
@@ -47,9 +47,9 @@ export class VictimConfirmLocOnMapPage implements OnInit {
         this.userInfo.emmergency = this.data.emmergency;
         this.userInfo.address = this.data.address;
         this.userInfo.phone_number = this.data.phone_number;
-      }else{  
+      } else {
         this.router.navigate(["/get-help"]);
-      }      
+      }
     });
   }
   ngOnInit() {
@@ -82,10 +82,18 @@ export class VictimConfirmLocOnMapPage implements OnInit {
         this.userInfo.latLong = this.markerlatlong;
         let request_ref = "23324256354";
         let responder_email = "tonykay001@gmail.com";
-        this._auth.addRequest(request_ref, this.userInfo.emmergency, this.userInfo.latLong.lat,
-          this.userInfo.latLong.long,this.userInfo.address, '',responder_email,this.userInfo.phone_number);
+        this._auth.addRequest(
+          request_ref,
+          this.userInfo.emmergency,
+          this.userInfo.latLong.lat,
+          this.userInfo.latLong.long,
+          this.userInfo.address,
+          "",
+          responder_email,
+          this.userInfo.phone_number
+        );
 
-        //this.router.navigate(["/unit-alert"]);
+        this.router.navigate(["/unit-alert"]);
       });
   }
 
@@ -128,12 +136,8 @@ export class VictimConfirmLocOnMapPage implements OnInit {
         google.maps.event.addListener(this.map.map, "dragend", () => {
           let center = this.map.map.getCenter();
           this.map.changeMarkerWithoutAni(center.lat(), center.lng());
-          console.log("Info1" , this.map.marker);
+          console.log("Info1", this.map.marker);
         });
-        
-       
       });
-
-      }
   }
-
+}
