b26126e79f1a63e485c5460d9a886a30db0e3d6a
TonykayNoni <tonykay001@gmail.com>
2020-06-06 18:33:23 +0000

    formatter

--- src/app/services/victims/request.service.ts
+++ src/app/services/victims/request.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
@@ -8,47 +8,47 @@ import "firebase/firestore";
 })
 
 export class RequestService {
-  
-  responders : any = [] ;
+
+  responders: any = [];
 
   constructor() { }
 
-  assignResponders(victim_id:string, request_ref:string, request_type:string, 
-    request_lat: number, request_long:number,request_address: string, 
-    respond_rating: string, victim_number:string, formatted_address: string ):Promise<any>{
+  assignResponders(victim_id: string, request_ref: string, request_type: string,
+    request_lat: number, request_long: number, request_address: string,
+    respond_rating: string, victim_number: string, formatted_address: string): Promise<any> {
     //run a query to find responders that are in the same geocoded location as the victim
     //essentially the same formatted address, which is stae evel in our case
     return firebase.firestore().collection('responder')
-    .where('formattedAddress', '==', formatted_address)
-    .where('respondantType', '==', request_type)
-    .get().then(result=>{
-      result.forEach(doc=>{
-        console.log(doc.data());
-        this.responders.push(doc.id);
-        //Here we will want to perform a sort of realtime notification
-        //Ideally, it would be a push notification
-       
-      });
-      //alert all these responders by populating the assigned_responders
-      //on the request
-      firebase.firestore().collection('request').where('request_ref','==',request_ref)
-      .limit(1).get().then(result=> {
-        result.forEach(doc=>{
-          firebase.firestore().collection('request').doc(`${doc.id}`).update({
-            assigned_responders: this.responders//firebase.firestore.FieldValue.arrayUnion(`${id}`)
-          });
-        })
-        
-      }, error=>{
+      .where('formattedAddress', '==', formatted_address)
+      .where('respondantType', '==', request_type)
+      .get().then(result => {
+        result.forEach(doc => {
+          console.log(doc.data());
+          this.responders.push(doc.id);
+          //Here we will want to perform a sort of realtime notification
+          //Ideally, it would be a push notification
+
+        });
+        //alert all these responders by populating the assigned_responders
+        //on the request
+        firebase.firestore().collection('request').where('request_ref', '==', request_ref)
+          .limit(1).get().then(result => {
+            result.forEach(doc => {
+              firebase.firestore().collection('request').doc(`${doc.id}`).update({
+                assigned_responders: this.responders//firebase.firestore.FieldValue.arrayUnion(`${id}`)
+              });
+            })
+
+          }, error => {
+            console.log(error);
+          })
+
+      }, error => {
         console.log(error);
-      })
-      
-    }, error=> {
-      console.log(error);
-    });
-    
+      });
+
   }
 
-  
+
 
 }
