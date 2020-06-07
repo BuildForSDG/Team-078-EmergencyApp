7e5f4c0f7d2c937bf32de3ae6197d5450d182846
TonykayNoni <tonykay001@gmail.com>
2020-06-07 18:50:19 +0000

    fix: Respondent view single request on Map closes: issue #91

---
+++ src/app/pages/respondant-pages/respondent-view-request-on-map/respondent-view-request-on-map.page.ts
@@ -0,0 +1,74 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { GoogleMapComponent } from '../../../components/google-map/google-map.component';
+const { Geolocation, Storage } = Plugins;
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-respondent-view-request-on-map',
+  templateUrl: './respondent-view-request-on-map.page.html',
+  styleUrls: ['./respondent-view-request-on-map.page.scss'],
+})
+export class RespondentViewRequestOnMapPage implements OnInit {
+  @ViewChild(GoogleMapComponent, { static: false })
+  map: GoogleMapComponent;
+  public loading: HTMLIonLoadingElement;
+  constructor(private alertCtrl: AlertController,
+  private loadingCtrl: LoadingController,private modalController: ModalController) { }
+  private latitude: number;
+  private longitude: number;
+  coord: any;
+  ngOnInit() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.map.init().then((res) => {
+          this.setLocation();
+        }, (err) => {
+          console.log(err);
+        });
+      }
+    });
+  }
+  async closeModal() {
+    await this.modalController.dismiss();
+    this.map.disableMap();
+  }
+
+  setLocation(): void {
+    this.loadingCtrl.create({
+      message: 'Checking current location...'
+    }).then((overlay) => {
+      overlay.present();
+      Geolocation.getCurrentPosition().then((position) => {
+        overlay.dismiss();
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        const currentLocation = {
+          lat : this.latitude,
+          lng : this.longitude
+        };
+        //this.map.mapLocation();
+        this.map.viewRequestOnMap(currentLocation,this.coord);
+        const data = {
+          latitude: this.latitude,
+          longitude: this.longitude
+        };
+        this.alertCtrl.create({
+          header: 'Location	confirmed!',
+          // message: 'You can now view any dangers on your current route.',
+          buttons: [{ text: 'Ok' }]
+        }).then((alert) => {
+          alert.present();
+        });
+      }, (err) => {
+        console.log(err);
+        overlay.dismiss();
+      });
+    });
+  }
+
+}
