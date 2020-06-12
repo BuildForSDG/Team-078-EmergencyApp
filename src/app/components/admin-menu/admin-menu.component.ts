1af545d946eb58660191040bc89a1ddbd9d3dc32
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-06-12 04:01:43 +0100

    fix: router autoload

--- src/app/components/admin-menu/admin-menu.component.ts
+++ src/app/components/admin-menu/admin-menu.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
-
+import { Component, OnInit } from "@angular/core";
+import { MenuController } from "@ionic/angular";
+import { Location } from "@angular/common";
+import {  Router } from "@angular/router";
 
 @Component({
-  selector: 'app-admin-menu',
-  templateUrl: './admin-menu.component.html',
-  styleUrls: ['./admin-menu.component.scss'],
+  selector: "app-admin-menu",
+  templateUrl: "./admin-menu.component.html",
+  styleUrls: ["./admin-menu.component.scss"],
 })
 export class AdminMenuComponent implements OnInit {
   
-
-  constructor(private location: Location) { 
-
-  }
+  constructor(private location: Location, 
+    private menu: MenuController, 
+    private route: Router) { }
   ngOnInit() {
+
+   }
+
+   openFirst() {
+    this.menu.enable(true, 'main-menu');
+    this.menu.open('main-menu');
   }
-  
+
   goBack() {
-  this.location.back();
-}
+    this.location.back();
+  }
 }
