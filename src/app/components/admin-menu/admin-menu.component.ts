a7fa11e6d7790f4a4b81354d209975e3709ccbd1
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-06-14 02:38:33 +0100

    fix: toggle issue

--- src/app/components/admin-menu/admin-menu.component.ts
+++ src/app/components/admin-menu/admin-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import {MenuController} from '@ionic/angular';
+import {Router, RouterEvent} from '@angular/router';
 
 @Component({
   selector: 'app-admin-menu',
@@ -7,15 +9,51 @@ import { Location } from '@angular/common';
   styleUrls: ['./admin-menu.component.scss'],
 })
 export class AdminMenuComponent implements OnInit {
-  
 
-  constructor(private location: Location) { 
+  pages = [
+    {
+      title: 'Add Danger',
+      url: '/admin-add-danger'
+    },
+    // {
+    //   title: 'View Responders',
+    //   url: '/view-respondant-list'
+    // },
+    {
+      title: 'Add Responders',
+      url: '/admin-add-danger'
+    },
+    {
+      title: 'Add Unit',
+      url: '/admin-add-unit'
+    },
+    {
+      title: 'View Dangers',
+      url: '/admin-view-dangers'
+    },
+    {
+      title: 'View Admin',
+      url: '/view-admins'
+    }
+  ];
 
+  selectedPath = '';
+
+  constructor(private location: Location, private menu: MenuController, private router: Router) { 
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.selectedPath = event.url;
+    });  
   }
   ngOnInit() {
+  }
+
+  toggleMenu() {
+    this.menu.toggle();
   }
   
   goBack() {
   this.location.back();
 }
 }
+
+// private menu: MenuController,
