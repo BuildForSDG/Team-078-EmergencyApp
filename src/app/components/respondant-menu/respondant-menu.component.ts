a2bbe92dcd8aefdb72fe4c6cebb1477bb4936691
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-06-14 15:20:11 +0100

    fix: Respondant menu Update

--- src/app/components/respondant-menu/respondant-menu.component.ts
+++ src/app/components/respondant-menu/respondant-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import {MenuController} from '@ionic/angular';
+import {Router, RouterEvent} from '@angular/router';
 
 @Component({
   selector: 'app-respondant-menu',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RespondantMenuComponent implements OnInit {
 
-  constructor() { }
+  pages = [
+    {
+      title: 'Admin Dashboard',
+      url: '/admin-dashboard',
+    },
+    {
+      title: 'Add Danger',
+      url: '/admin-add-danger',
+    },
+    // this Keeps spooling to infinity
+    // {
+    //   title: 'View Responders',
+    //   url: '/view-respondant-list',
+    // },
+    {
+      title: 'Add Responders',
+      url: '/admin-add-respondant',
+    },
+    {
+      title: 'Add Unit',
+      url: '/admin-add-unit',
+    },
+    // this Keeps spooling to infinity
+    // {
+    //   title: 'View Dangers',
+    //   url: '/admin-view-dangers',
+    // },
+    {
+      title: 'View Admin',
+      url: '/view-admins',
+    },
+  ];
 
+  selectedPath = '';
+
+  constructor(
+    private location: Location,
+    private menu: MenuController,
+    private router: Router
+  ) {
+    this.router.events.subscribe(
+      (event: RouterEvent) => {
+        this.selectedPath = event.url;
+      }
+    );
+  }
   ngOnInit() {}
+
+  toggleMenu() {
+    this.menu.toggle('main-menu');
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
 
 }
 
