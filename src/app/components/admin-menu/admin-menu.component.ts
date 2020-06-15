e028a908ad912087ba096465fe85e58f6a631987
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-06-15 13:07:39 +0100

    fix: menu toggle fix

--- src/app/components/admin-menu/admin-menu.component.ts
+++ src/app/components/admin-menu/admin-menu.component.ts
@@ -1,75 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
-import {MenuController} from '@ionic/angular';
-import {Router, RouterEvent} from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
+import { MenuController } from "@ionic/angular";
+import { Router, RouterEvent } from "@angular/router";
 
 @Component({
-  selector: 'app-admin-menu',
-  templateUrl: './admin-menu.component.html',
-  styleUrls: ['./admin-menu.component.scss'],
+  selector: "app-admin-menu",
+  templateUrl: "./admin-menu.component.html",
+  styleUrls: ["./admin-menu.component.scss"],
 })
-export class AdminMenuComponent
-  implements OnInit {
+export class AdminMenuComponent implements OnInit {
   pages = [
     {
-      title: 'Admin Dashboard',
-      url: '/admin-dashboard',
-      icon: 'clipboard-outline'
+      title: "Admin Dashboard",
+      url: "/admin-dashboard",
+      icon: "clipboard-outline",
     },
     {
-      title: 'Add Danger',
-      url: '/admin-add-danger',
-      icon: "flame-outline"
-    },
-    // this Keeps spooling to infinity
-    // {
-    //   title: 'View Responders',
-    //   url: '/view-respondant-list',
-    // },
-    {
-      title: 'Add Responders',
-      url: '/admin-add-respondant',
-      icon: 'person-add-outline'
+      title: "Add Danger",
+      url: "/admin-add-danger",
+      icon: "flame-outline",
     },
     {
-      title: 'Add Unit',
-      url: '/admin-add-unit',
-      icon: 'location-outline'
+      title: "View Dangers",
+      url: "/admin-view-dangers",
+      icon: "bonfire-outline",
     },
-    // this Keeps spooling to infinity
-    // {
-    //   title: 'View Dangers',
-    //   url: '/admin-view-dangers',
-    // },
     {
-      title: 'View Admin',
-      url: '/view-admins',
-      icon: 'eye-outline'
+      title: "Add Responders",
+      url: "/admin-add-respondant",
+      icon: "person-add-outline",
+    },
+    {
+      title: "View Respondents",
+      url: "/admin-view-dangers",
+      icon: "eye-outline",
+    },
+    {
+      title: "Add Unit",
+      url: "/admin-add-unit",
+      icon: "location-outline",
     },
   ];
 
+  selectedPath = "";
 
-  selectedPath = '';
+  isShowDiv: boolean = false;
 
   constructor(
     private location: Location,
     private menu: MenuController,
     private router: Router
   ) {
-    this.router.events.subscribe(
-      (event: RouterEvent) => {
-        this.selectedPath = event.url;
-      }
-    );
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.selectedPath = event.url;
+    });
   }
   ngOnInit() {}
 
-  toggleMenu() {
-    this.menu.toggle('main-menu');
+  toggleDisplayDiv() {
+    this.isShowDiv = !this.isShowDiv;
   }
 
   goBack() {
     this.location.back();
   }
 }
-
